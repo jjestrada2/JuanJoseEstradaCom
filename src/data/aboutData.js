247893824c091cc16e7ca25898233edc5ab0f552
Juan Jose Estrada <juan5801331@gmail.com>
2022-06-08 13:55:39 -0700

    who I am?

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Jane. I'm a web designer and developer based in Southampton, UK.",
-    description2: "During the day I work as a lead developer at a local agency and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
+    description1: "My name's Juan. I'm a web developer based in San Francisco, US.",
+    description2: "During the day I work 👨🏻‍🔬 on freelance projects 👾 I love entrepreneurship and create useful stuff. I spent my leisure hours reading world leaders' biographies🧙🏻‍♀️. Right now I'm pursuing a computer science degree here in Silicon Valley.👨🏻‍🎓",
     image: 2
 }

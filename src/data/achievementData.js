55ae1645e7e504c0b95f8a1c230b280fb58fa584
Juan Jose Estrada <juan5801331@gmail.com>
2022-06-11 10:31:32 -0700

    achievements

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -1,29 +1,41 @@
+import jsCertificate from "../assets/certificates/diploma-javascript-profesional.webp"
+import isCertificate from "../assets/certificates/diploma-ingenieria.webp"
+import pooCertificate from "../assets/certificates/diploma-oop.webp"
+
 export const achievementData = {
-    bio : "Putting quantified achievements on a resume is great, but I just don’t work with hard numbers. I have no data to support the quality of my work. If you have lots of relevant experience, the heading statement of your resume will be a summary.",
+    bio : "",
     achievements : [
         {
             id : 1,
-            title : 'Google Cloud certification',
-            details : 'Google Cloud certifications validate your expertise and showcase your ability to transform businesses with Google Cloud technology.',
-            date : 'Aug 20, 2019',
-            field : 'Automation',
-            image : 'https://images.unsplash.com/photo-1573141597928-403fcee0e056?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80'
+            title : 'JavaScript Professional Course',
+            details : 'Platzi',
+            date : 'Sept 22 , 2021',
+            field : 'Programing',
+            image : jsCertificate
         },
         {
             id : 2,
-            title : 'AWS Certification',
-            details : 'Validate technical skills and cloud expertise to grow your career and business.',
-            date : 'Aug 20, 2019',
-            field : 'Automation',
-            image : 'https://images.unsplash.com/photo-1523474253046-8cd2748b5fd2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+            title : 'software engineering fundamentals',
+            details : 'Plati',
+            date : 'Sept 22 , 2021',
+            field : 'Programing',
+            image : isCertificate
         },
         {
             id : 3,
-            title : 'Professional Data Engineer Certification',
-            details : 'Professional Data Engineers enable data-driven decision making by collecting, transforming, and publishing data.',
-            date : 'Aug 20, 2019',
-            field : 'Automation',
-            image : 'https://images.unsplash.com/photo-1489389944381-3471b5b30f04?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+            title : 'Object-oriented programming',
+            details : 'Platzi',
+            date : 'Aug 20, 2021',
+            field : 'Programing',
+            image : pooCertificate
+        },
+        {
+            id : 4,
+            title : 'Duolingo english test',
+            details : '(Score Doulingo:105) (IELTS:6.5) (CEFR:B2)',
+            date : 'March 30, 2021',
+            field : 'English',
+            image : "https://d23cwzsbkjbm45.cloudfront.net/static/images/wp/cea9d9ec97e008b565795bbda7e1d686.svg"
         }
 
     ]

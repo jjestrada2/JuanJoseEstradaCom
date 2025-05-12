7e9dbf35c4576fbef43edd4319fc5575a66f702a
Juan José Estrada <69777842+jjestrada2@users.noreply.github.com>
2025-05-12 13:27:28 -0700

    Update contactsData.js

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
-    email: 'jjestradafreelancer@gmail.com',
-    phone: '+1 4154257833',
-    address: '231 N Claremont St San Mateo CA',
+    email: '',
+    phone: '',
+    address: ' 415 Mission Street,San Francisco CA',
 
     sheetAPI: 'https://sheetdb.io/api/v1/atpx5uuknmll2'
 }
